0e2ce1bb29cdf153af4cbe063ef077b9703cd141
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-06-29 19:13:39 -0400

    Add buttons.

--- AishaKleemoff-Inventory-Web-App-Project/main.js
+++ AishaKleemoff-Inventory-Web-App-Project/main.js
@@ -1,1 +1,12 @@
-console.log("Aisha is a Software Developer")
+console.log("Aisha is a Software Developer");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const price = document.querySelector("#price");
+  console.log(price.value);
+
+  const author = document.querySelector("#author");
+  console.log(author.value);
+});
